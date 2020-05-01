5039939a2c99bd842012c340a717a0616a889e98
Adam Notodikromo <adam.pahlevi@gmail.com>
2020-05-01 21:39:16 +0900

    refactor ruler

--- lib/bali/statics/scope_ruler.rb
+++ lib/bali/statics/scope_ruler.rb
@@ -1,27 +1,38 @@
 module Bali::Statics::ScopeRuler
-  def rule_scope(arg1, arg2 = nil)
-    if arg2.nil?
-      data = arg1
-      if respond_to?(:current_user)
-        actor = current_user
+  module HelperFunctions
+    extend self
+
+    def extract_data_and_actor(obj, arg1, arg2 = nil)
+      if arg2.nil?
+        data = arg1
+        if obj.respond_to?(:current_user)
+          actor = obj.current_user
+        end
+      else
+        data, actor = arg1, arg2
       end
-    else
-      data, actor = arg1, arg2
+
+      return data, actor
     end
 
+    def scope_for(relation)
+      rule_class = Bali::Rules.for(relation.model)
+      return unless rule_class
+
+      rule_class.inheritable_role.scope
+    end
+  end
+
+  def rule_scope(arg1, arg2 = nil)
+    data, actor = HelperFunctions.extract_data_and_actor(self, arg1, arg2)
     return unless data
 
-    rule_class = Bali::Rules.for(data.model)
-    return unless rule_class
-    scope = rule_class.inheritable_role.scope
-
-    if scope.arity == 0
-      scoped_data = scope.call
-    elsif scope.arity == 1
-      scoped_data = scope.call(data)
-    elsif scope.arity == 2
-      scoped_data = scope.call(data, actor)
-    end
+    scope = HelperFunctions.scope_for(data)
+    scoped_data = case scope.arity
+                  when 0 then scope.call
+                  when 1 then scope.call(data)
+                  when 2 then scope.call(data, actor)
+                  end
 
     scoped_data || data
   end
